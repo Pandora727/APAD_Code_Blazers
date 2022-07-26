bb6e57636d050ab9e75d26020dcde9750673a82f
Mahathi <mahathireddy.1998@gmail.com>
2022-07-26 13:19:51 -0500

    initial code commit

---
+++ code_blazers_backend/user.py
@@ -0,0 +1,16 @@
+class User:
+    def __init__(self):
+        pass
+    def set_userid(self, userid: str):
+        self.user_id = userid
+
+    def set_pwd(self, pwd: str):
+        self.pwd = pwd
+    
+    def user_login(self):
+        pass 
+
+    def create_newuser(self):
+        pass
+
+
